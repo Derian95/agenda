3d095455ab87ea9abe46c9e21c8cceb39faff786
derian95 <dfhamezquita@gmail.com>
2023-04-23 16:29:57 -0500

    another api url

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 
 export const traerEventos=async() => {
-    const respuesta = await axios.get('https://agenda-upt.herokuapp.com/api/event/getAllEvents')
+    const respuesta = await axios.get('https://agend-aupt.onrender.com/api/event/getAllEvents')
     return(respuesta.data)
 } 
